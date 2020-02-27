e56d82c3566a64ed85ccbf0a6c84aba69c5a4836
tinka <tinkah8@gmail.com>
2020-02-27 15:28:53 +0000

    Přidány views pro vytvoření a editaci příspěvku.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, re_path
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    #re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail')
+    path('post/new/', views.post_new, name="post_new"),
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
 ]
