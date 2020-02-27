a939267448320e59deacfaec070386cd546c2522
tinka <tinkah8@gmail.com>
2020-02-27 10:40:10 +0000

    Přidáno view pro detail příspěvku a CSS pro stránky.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 
 from . import views
-from django.urls import path
+from django.urls import path, re_path
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #re_path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail')
 ]
